76abdfd32579488de7f8dfc08f46cefa3cc81f1a
shravni21 <wakdeshravni1@gmail.com>
2024-02-26 12:26:47 +0530

    second commit

--- 01-spring-boot-overview/01-spring-boot-demo/src/main/java/com/spring/quickstart/Rest/HelloWorldController.java
+++ 01-spring-boot-overview/01-spring-boot-demo/src/main/java/com/spring/quickstart/Rest/HelloWorldController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+    // Rest controller
     @GetMapping(path = "/hello")
 
     public String HelloWorld(){
---
+++ 01-spring-boot-overview/02-dev-tools-demo/src/test/java/com/spring/quickstart/QuickstartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.quickstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickstartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
