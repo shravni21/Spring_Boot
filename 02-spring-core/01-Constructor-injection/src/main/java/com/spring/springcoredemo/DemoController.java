7431a94044f00eb8c6bbbadd5a8a7dff983ec45d
shravni21 <wakdeshravni1@gmail.com>
2024-02-27 13:12:12 +0530

    Component scanning

---
+++ 02-spring-core/01-Constructor-injection/src/main/java/com/spring/springcoredemo/DemoController.java
@@ -0,0 +1,2 @@
+package com.spring.springcoredemo;public class DemoController {
+}
---
+++ 02-spring-core/02-Component-scanning/src/main/java/com/spring/springcoredemo/rest/DemoController.java
@@ -0,0 +1,23 @@
+package com.spring.springcoredemo.rest;
+
+import com.spring.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    // Define private field for the dependency
+    private Coach mycoach;
+
+    //Define constructor for dependency injection
+    @Autowired // if only one constructor the its optional
+    public DemoController(Coach theCoach) {
+        mycoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout(){
+        return mycoach.getDailiyWorkout();
+    }
+}
