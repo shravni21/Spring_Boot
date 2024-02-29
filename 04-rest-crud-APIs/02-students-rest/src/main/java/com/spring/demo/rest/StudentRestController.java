9dfa037519d0d34a02de8214c1d2ec89ca3d033d
shravni21 <wakdeshravni1@gmail.com>
2024-02-29 13:46:35 +0530

    Add construct

--- 04-rest-crud-APIs/02-students-rest/src/main/java/com/spring/demo/rest/StudentRestController.java
+++ 04-rest-crud-APIs/02-students-rest/src/main/java/com/spring/demo/rest/StudentRestController.java
@@ -1,6 +1,7 @@
 package com.spring.demo.rest;
 
 import com.spring.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,15 +12,23 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
-    //Define endpoint for "/students" - return the list of students
-    @GetMapping("/students")
-    public List<Student> getStudent() {
-        List<Student> theStudent = new ArrayList<>();
+
+    private List<Student> theStudent;
+
+    //Define @PostConstruct to load the student data
+    @PostConstruct
+    public void loadData() {
+        theStudent = new ArrayList<>();
+
         theStudent.add(new Student("Pornima", "Patel"));
         theStudent.add(new Student("Shravni", "Wakde"));
         theStudent.add(new Student("Nandini", "Indrale"));
         theStudent.add(new Student("Shweta", "Zade"));
+    }
 
+    //Define endpoint for "/students" - return the list of students
+    @GetMapping("/students")
+    public List<Student> getStudent() {
         return theStudent;
     }
 
