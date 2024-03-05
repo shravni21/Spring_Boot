59446e49dfa321a93f40dc34171287d6c092a185
shravni21 <wakdeshravni1@gmail.com>
2024-03-05 15:38:12 +0530

    form validation

---
+++ 06-spring-boot-mvc/03-mvc-validation-demo/src/main/java/com/springboot/mvc/ValidatiobDemoApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidatiobDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidatiobDemoApplication.class, args);
+	}
+
+}
