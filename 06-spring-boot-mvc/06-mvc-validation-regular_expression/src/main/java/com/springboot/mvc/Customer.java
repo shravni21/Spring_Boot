f1ca1b2bb7c04491501ff3dabc5e57512358ddfd
shravni21 <wakdeshravni1@gmail.com>
2024-03-05 17:21:12 +0530

    making integer field required

--- 06-spring-boot-mvc/06-mvc-validation-regular_expression/src/main/java/com/springboot/mvc/Customer.java
+++ 06-spring-boot-mvc/06-mvc-validation-regular_expression/src/main/java/com/springboot/mvc/Customer.java
@@ -6,11 +6,11 @@ public class Customer {
     private String firstName;
     @NotNull(message = "is required")
     @Size(min = 1, message = "is required")
-
     private String lastName;
+    @NotNull(message = "is required")
     @Min(value = 0, message = "Must be greater than of equal to zero.")
     @Max(value = 10, message = "Must be greater than or equal to 10")
-    private int freePasses;
+    private Integer freePasses;
 
     @Pattern(regexp = "^[0-9]{6}", message = "Only 6 digits")
     private String postalCode;
@@ -39,11 +39,11 @@ public class Customer {
         this.lastName = lastName;
     }
 
-    public int getFreePasses() {
+    public Integer getFreePasses() {
         return freePasses;
     }
 
-    public void setFreePasses(int freePasses) {
+    public void setFreePasses(Integer freePasses) {
         this.freePasses = freePasses;
     }
 }
