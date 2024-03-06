1773e8a07a805474ce59e522199bc240c9d3066b
shravni21 <wakdeshravni1@gmail.com>
2024-03-06 13:22:01 +0530

    gitignore

---
+++ 04-rest-crud-APIs/Job_web_project/src/main/java/com/spring/job_web_project/JobWebProjectApplication.java
@@ -0,0 +1,13 @@
+package com.spring.job_web_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobWebProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobWebProjectApplication.class, args);
+    }
+
+}
---
+++ 04-rest-crud-APIs/Job_web_project/src/test/java/com/spring/job_web_project/JobWebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.job_web_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobWebProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
