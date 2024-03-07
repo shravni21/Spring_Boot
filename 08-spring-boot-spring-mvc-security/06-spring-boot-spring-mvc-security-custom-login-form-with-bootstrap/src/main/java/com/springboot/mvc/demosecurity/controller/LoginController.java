41305824c1e9cccfca7adb1877f3987d2c89b35e
shravni21 <wakdeshravni1@gmail.com>
2024-03-07 17:16:11 +0530

    custom login form with bootstrap

--- 08-spring-boot-spring-mvc-security/06-spring-boot-spring-mvc-security-custom-login-form-with-bootstrap/src/main/java/com/springboot/mvc/demosecurity/controller/LoginController.java
+++ 08-spring-boot-spring-mvc-security/06-spring-boot-spring-mvc-security-custom-login-form-with-bootstrap/src/main/java/com/springboot/mvc/demosecurity/controller/LoginController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class LoginController {
     @GetMapping("showMyLoginPage")
     public String showMyLoginPage() {
-        return "plain-login";
+        return "fancy-login";
+//        return "plain-login";
     }
 }
