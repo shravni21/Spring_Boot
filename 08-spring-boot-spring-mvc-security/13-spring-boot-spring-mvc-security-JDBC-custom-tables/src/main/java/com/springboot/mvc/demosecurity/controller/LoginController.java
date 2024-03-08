f44254de60d76b199ec1890c4bea368459ed12fc
shravni21 <wakdeshravni1@gmail.com>
2024-03-08 15:59:57 +0530

    custom tables

---
+++ 08-spring-boot-spring-mvc-security/13-spring-boot-spring-mvc-security-JDBC-custom-tables/src/main/java/com/springboot/mvc/demosecurity/controller/LoginController.java
@@ -0,0 +1,20 @@
+package com.springboot.mvc.demosecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("showMyLoginPage")
+    public String showMyLoginPage() {
+        return "fancy-login";
+//        return "plain-login";
+    }
+
+    // Add request mapping for /access-denied
+    @GetMapping("/access-denied")
+    public String showAccessDenied(){
+        return "access-denied";
+    }
+}
+
