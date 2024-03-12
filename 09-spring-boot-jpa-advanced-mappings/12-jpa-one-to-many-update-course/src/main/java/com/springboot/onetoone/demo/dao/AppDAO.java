337e4985030c8337baadcfa226542e858e9aa095
shravni21 <wakdeshravni1@gmail.com>
2024-03-12 12:45:19 +0530

    update course

---
+++ 09-spring-boot-jpa-advanced-mappings/12-jpa-one-to-many-update-course/src/main/java/com/springboot/onetoone/demo/dao/AppDAO.java
@@ -0,0 +1,29 @@
+package com.springboot.onetoone.demo.dao;
+
+import com.springboot.onetoone.demo.entity.Course;
+import com.springboot.onetoone.demo.entity.Instructor;
+import com.springboot.onetoone.demo.entity.InstructorDetail;
+
+import java.util.List;
+
+public interface AppDAO {
+    void save(Instructor theInstructor);
+
+    Instructor findInstructorById(int theId);
+
+    void deleteInstructorById(int theId);
+
+    InstructorDetail findInstructorDetailById(int theId);
+
+    void deleteInstructorDetailById(int theId);
+
+    List<Course> findCoursesByInstructorId(int theId);
+
+    Instructor findInstructorByIdJoinFetch(int theId);
+
+    void update(Instructor tempInstructor);
+
+    void update(Course tempCourse);
+
+    Course findCourseById(int theId);
+}
