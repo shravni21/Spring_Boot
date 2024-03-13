fd95c84490593574c50827c8d22c734a38ba3215
shravni21 <wakdeshravni1@gmail.com>
2024-03-13 16:07:49 +0530

    match any method starting with add

---
+++ 10-spring-boot-aop/04-spring-boot-aop-pointcut-expressions-match-any-add-start-method/src/main/java/com/springboot/demo/DemoApplication.java
@@ -0,0 +1,48 @@
+package com.springboot.demo;
+
+import com.springboot.demo.dao.AccountDAO;
+import com.springboot.demo.dao.MembershipDAO;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(AccountDAO theAccountDAO, MembershipDAO theMembershipDAO) {
+        return runner -> {
+            demoTheBeforeAdvice(theAccountDAO, theMembershipDAO);
+
+        };
+    }
+
+    private void demoTheBeforeAdvice(AccountDAO theAccountDAO, MembershipDAO theMembershipDAO) {
+
+        // call the business method
+        theAccountDAO.addAccount();
+
+        // call the membership business method
+        theMembershipDAO.addAccount();
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
