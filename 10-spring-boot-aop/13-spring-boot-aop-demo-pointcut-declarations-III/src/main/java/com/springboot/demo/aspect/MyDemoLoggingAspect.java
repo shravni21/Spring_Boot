3e87aea1cea1d902ce703fafe87ce9d9863f99e0
shravni21 <wakdeshravni1@gmail.com>
2024-03-14 14:01:13 +0530

    combine ointcut

---
+++ 10-spring-boot-aop/13-spring-boot-aop-demo-pointcut-declarations-III/src/main/java/com/springboot/demo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,43 @@
+package com.springboot.demo.aspect;
+
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class MyDemoLoggingAspect {
+
+    @Pointcut("execution(* com.springboot.demo.dao.*.*(..))")
+    private void forDaoPackage() {
+    }
+
+
+    // create a pointcut for getter methods
+    @Pointcut("execution(* com.springboot.demo.dao.*.get*(..))")
+    private void getter() {
+    }
+
+    // create pointcut for setter methods
+    @Pointcut("execution(* com.springboot.demo.dao.*.set*(..))")
+    private void setter() {
+    }
+
+    // create pointcut: include package ...exclude getter/setter
+    @Pointcut("forDaoPackage() && !(getter()||setter())")
+    private void forDaoPackageNoGetterSetter() {
+    }
+
+    @Before("forDaoPackageNoGetterSetter()")
+
+    public void beforeAddAccountAdvice() {
+        System.out.println("\n  ........Executing @Before advice on method........");
+    }
+
+    @Before("forDaoPackageNoGetterSetter()")
+    public void performApiAnalytics() {
+        System.out.println("\n =======> Performing API analytics!!");
+    }
+}
